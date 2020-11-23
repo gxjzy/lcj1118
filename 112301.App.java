14e22656831e0c52dada3360bb23fc769b0de015
gxjzy <74659882+gxjzy@users.noreply.github.com>
2020-11-23 11:45:09 +0800

    Update 112301.App.java

--- 112301.App.java
+++ 112301.App.java
@@ -4,13 +4,14 @@ public class App {
     public static void main(String[] args) {
         while (true) {
             Scanner scanner = new Scanner(System.in);
-            System.out.print("请您输入数字:");
+            System.out.print("请您输入1:");
             int i = scanner.nextInt();
 
             int max = 100, min = 1;
             int ran2 = (int) (Math.random() * (max - min) + min);
+            int ran3 = (int) (Math.random() * (max - min) + min);
 
-            System.out.println(i + "," + ran2);
+            System.out.println(ran2 + "," + ran3);
         }
     }
 }
