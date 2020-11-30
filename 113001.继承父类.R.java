e323549f39ae0e4763e04550407fbe4f76c8643a
gxjzy <74659882+gxjzy@users.noreply.github.com>
2020-11-30 11:38:43 +0800

    Create 113001.继承父类.R.java

---
+++ 113001.继承父类.R.java
@@ -0,0 +1,11 @@
+public class R {
+	static void A(){
+		String boy_filePath="img/boy.gif";
+		String girl_filePath="img/girl.gif";
+		int [][]arr={{1,2,3},{4,5,6}};
+		
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		System.out.println(arr);
+	}
+	}
