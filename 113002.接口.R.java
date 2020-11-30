5941d22cf721d234bb0f83fa868d5016d77b63b3
gxjzy <74659882+gxjzy@users.noreply.github.com>
2020-11-30 11:39:52 +0800

    Create 113002.接口.R.java

---
+++ 113002.接口.R.java
@@ -0,0 +1,11 @@
+public interface I {
+	static void A(){
+		String boy_filePath="img/boy.gif";
+		String girl_filePath="img/girl.gif";
+		int [ ][ ]array2d={{1,2,3},{4,5,6}};
+		
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		System.out.println(array2d);
+	}
+}
